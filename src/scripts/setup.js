886f2b8992489bb5d5821fc2cb292645a6842945
sn2 <mr_ralla@yahoo.com>
2023-10-04 11:47:49 +1000

    Added fallback to WebGL, cleaned up useEffects

--- src/scripts/setup.js
+++ src/scripts/setup.js
@@ -20,140 +20,125 @@ import {
     Space,
 } from "@babylonjs/core"
 import "@babylonjs/loaders"
-//import { init } from './checkgpu'
-
 
 
 const BabylonComponent = ({ hideflags }) => {
 
-    const [scene, setScene] = useState(null)
-
-    const [GPUengine, setGPUengine] = useState({})
+    let scene = {}
     const [babylonObjects, setBabylonObjects] = useState([])
-    const renderCanvas = useRef(null);
+    const renderCanvas = useRef(null)
+    const webGL = useRef(false)
     let resizeTimeout
-    let windowSize = useRef([]);
-    const orange = useRef({})
-    //let orange = {}
+    let windowSize = []
+    let orange = {}
+    
+    const sceneBuild = (engine, canvas) => {
+        //scene
+        scene = new Scene(engine)
 
+        //camera
+        var camera = new ArcRotateCamera("camera", Tools.ToRadians(45), Tools.ToRadians(65), 10, Vector3.Zero(), scene)
+        camera.setTarget(Vector3.Zero())
+        camera.attachControl(canvas, true)
+        //lights
+        var light = new HemisphericLight("light1", new Vector3(0, 1, 1), scene)
+        light.intensity = 0.7
+
+        //materials
+        var pbr = new PBRMaterial()
+        pbr.albedoColor = new Color3(1, 0.5, 0)
+        pbr.metallic = 0.1
+        pbr.roughness = 0.1
+        orange.current = pbr
+
+        var brown = orange.current.clone("brownMaterial")
+        brown.albedoColor = new Color3(0.7, 0.1, 0)
+
+        //3d objects
+        var sphere = CreateSphere("sphere1", { segments: 16, diameter: 2 }, scene)
+        sphere.position.y = 2
+        sphere.material = pbr
+
+        // ground
+        var ground = CreateGround("ground1", { width: 256, height: 256, subdivisions: 2 }, scene)
+        ground.material = brown
+
+        //environment 
+        const skybox = MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene)
+        const skyboxMaterial = new StandardMaterial("skyBox", scene)
+        skyboxMaterial.backFaceCulling = false
+        skyboxMaterial.reflectionTexture = new CubeTexture("./media/textures/skybox/skybox", scene)
+        skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE
+        skyboxMaterial.diffuseColor = new Color3(0, 0, 0)
+        skyboxMaterial.specularColor = new Color3(0, 0, 0)
+        skybox.material = skyboxMaterial
+
+        //custom mesh loading
+        var building = {}
+        loadMeshes(building, "./media/", "building.glb", scene)
+
+        setBabylonObjects([...babylonObjects, ground, camera, building])
+
+        //resize engine render on window resize release
+        const handleResize = () => {
+            clearTimeout(resizeTimeout)
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            resizeTimeout = setTimeout(() => {
+                windowSize.current = [window.innerWidth, window.innerHeight]
+                engine.resize()
+            }, 200)
+        }
+        window.addEventListener('resize', handleResize)
+
+        engine.runRenderLoop(() => {
+            scene.render()
+        })
+
+        return () => {
+            engine.dispose()
+        }
+    }
+
+    //webGPU engine
+    const createSceneGPU = async () => {
+        const { current: canvas } = renderCanvas
+        const engine = new WebGPUEngine(canvas)
+        await engine.initAsync()
+        sceneBuild(engine, canvas)
+    }
+
+    //webGPU engine
+    const createSceneWebGL = () => {
+        const { current: canvas } = renderCanvas
+        const engine = new Engine(canvas, true)
+        sceneBuild(engine, canvas)
+    }
+
+    //entry point
     useEffect(() => {
-
-        const sceneBuild = (engine, canvas) => {
-            //scene
-            const newScene = new Scene(engine)
-
-            //camera
-            var camera = new ArcRotateCamera("camera", Tools.ToRadians(45), Tools.ToRadians(65), 10, Vector3.Zero(), newScene)
-            camera.setTarget(Vector3.Zero())
-            camera.attachControl(canvas, true)
-            //lights
-            var light = new HemisphericLight("light1", new Vector3(0, 1, 1), newScene)
-            light.intensity = 0.7
-
-            //materials
-            var pbr = new PBRMaterial()
-            pbr.albedoColor = new Color3(1, 0.5, 0)
-            pbr.metallic = 0.1
-            pbr.roughness = 0.1
-            orange.current = pbr
-            //orange = pbr
-
-            var brown = orange.current.clone("brownMaterial");
-            brown.albedoColor = new Color3(0.7, 0.1, 0);
-
-            //3d objects
-            var sphere = CreateSphere("sphere1", { segments: 16, diameter: 2 }, newScene)
-            sphere.position.y = 2
-            sphere.material = pbr
-
-            // ground
-            var ground = CreateGround("ground1", { width: 256, height: 256, subdivisions: 2 }, newScene)
-            ground.material = brown
-
-            //environment 
-            const skybox = MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, newScene);
-            const skyboxMaterial = new StandardMaterial("skyBox", newScene);
-            skyboxMaterial.backFaceCulling = false;
-            skyboxMaterial.reflectionTexture = new CubeTexture("./media/textures/skybox/skybox", newScene);
-            skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
-            skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
-            skyboxMaterial.specularColor = new Color3(0, 0, 0);
-            skybox.material = skyboxMaterial;
-
-            //custom mesh loading
-            var building = {}
-            loadMeshes(building, "./media/", "building.glb", newScene)
-            setGPUengine(engine)
-            setScene(newScene)
-            setBabylonObjects([...babylonObjects, ground, camera, building]);
-
-            //resize engine render on window resize release
-            const handleResize = () => {
-                clearTimeout(resizeTimeout);
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                resizeTimeout = setTimeout(() => {
-                    windowSize.current = [window.innerWidth, window.innerHeight];
-                    engine.resize();
-                }, 200);
-            };
-            window.addEventListener('resize', handleResize);
-
-            return () => {
-                engine.dispose()
-                GPUengine.dispose() // Cleanup the engine when the component unmounts
-            }
-
-        }
-        //webGPU engine
-        const createSceneGPU = async () => {
-            const { current: canvas } = renderCanvas
-            const engine = new WebGPUEngine(canvas)
-            await engine.initAsync()
-            sceneBuild(engine, canvas)
-        }
-
-        //webGPU engine
-        const createSceneWebGL =() => {
-            const { current: canvas } = renderCanvas
-            const engine = new Engine(canvas, true)
-            sceneBuild(engine, canvas)
-        }
-
-        if (!scene) {
+        if (Object.keys(scene).length === 0) {
             if (!navigator.gpu) {
                 console.warn("WebGPU not supported.")
                 console.warn("Switching to WebGL")
                 createSceneWebGL() //with webGL
+                webGL.current = true
             }
             else {
                 console.log("Using WebGPU")
                 createSceneGPU() //with webGPU
             }
-
         }
 
-    }, [])
+    },[])
 
-    if (scene) {
-        GPUengine.runRenderLoop(() => {
-            //  rotateRender(babylonObjects[0])
-            scene.render()
-        })
+     //show or hide meshes
+    if (babylonObjects[2]) {
+        let checkvalid = false //HACk had to check if each object are valid first, workaroud error
+        babylonObjects[2].Frame ? babylonObjects[2].Frame.isVisible = hideflags.frame : checkvalid = true
+        babylonObjects[2].windowsmesh ? babylonObjects[2].windowsmesh.isVisible = hideflags.window : checkvalid = true
+        babylonObjects[2].door ? babylonObjects[2].door.isVisible = hideflags.door : checkvalid = true
+        babylonObjects[2].Wall ? babylonObjects[2].Wall.isVisible = hideflags.wall : checkvalid = true
     }
-
-    //show or hide meshes
-    useEffect(() => {
-
-        if (babylonObjects[2]) {
-            let x = 0
-            babylonObjects[2].Frame ? babylonObjects[2].Frame.isVisible = hideflags.frame : x = true
-            babylonObjects[2].windowsmesh ? babylonObjects[2].windowsmesh.isVisible = hideflags.window : x = true
-            babylonObjects[2].door ? babylonObjects[2].door.isVisible = hideflags.door : x = true
-            babylonObjects[2].Wall ? babylonObjects[2].Wall.isVisible = hideflags.wall : x = true
-        }
-    }, [hideflags])
-
-
 
     function loadMeshes(obj, path, filename, scene) {
         // obj.file = await SceneLoader.AppendAsync("")
@@ -179,32 +164,14 @@ const BabylonComponent = ({ hideflags }) => {
                 ref={renderCanvas}
                 id="renderCanvas"
             >
-            </canvas>
+            </canvas> 
+            <p id="renderType">{webGL.current? "Browser or GPU not supported, falling back to WebGL from WebGPU":"Running on WebGPU"}</p>
         </div>
     )
 }
 
 export default BabylonComponent
 
-function rotateRender(mesh) {
-    mesh.rotate(new Vector3(0, 1, 0), 0.002)
-}
 
-// function loadMeshes(obj, path, filename, scene) {
-//     // obj.file = await SceneLoader.AppendAsync("")
-//     obj.file = SceneLoader.ImportMesh("", path, filename, scene, (meshes, ps, sk, ani) => {
-//         console.log("Meshes", meshes)
-//         console.log("Ani", ani)
-//         let side = 0
-//         meshes.forEach(element => {
-//             obj[element.name] = scene.getMeshByName(element.name)
-//             console.log(element.name)
-//             element.material = orange
-//             element.scaling = new Vector3(2, 2, 2)
-//             element.rotate(new Vector3(0, 1, 0), Tools.ToRadians(side), Space.WORLD)
-//             side = side + 90
-//         })
-//         console.log(obj)
-//     })
-// }
+
 
